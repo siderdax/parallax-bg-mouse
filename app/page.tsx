0bf02329c414ae4a552b657820417ac18883c2bf
KYI <headwaving@gmail.com>
2023-09-29 22:28:18 +0900

    scroll event+

--- app/page.tsx
+++ app/page.tsx
@@ -12,32 +12,51 @@ export default function Home() {
       })
     })
 
+    document.querySelector('.main')?.addEventListener('scroll', (e: Event) => {
+      const el = e.target as Element
+      document.querySelectorAll<HTMLElement>('.gal-scroll').forEach((box) => {
+        const dataValue = parseInt(box.getAttribute('data-value') ?? '0')
+        const transform =
+          'translateY(' + (el.scrollTop * dataValue) / 250 + 'px) '
+        box.style.transform = transform
+        box.style.transitionDuration = '0'
+      })
+    })
+
     return () => {}
   }, [])
 
   return (
     <>
       <section
-        className='to-purple-800 to-100%" absolute h-full
-        w-full bg-gradient-to-b from-black from-50% via-indigo-900 via-90%'
+        className='to-100%" absolute h-full w-full
+        bg-gradient-to-b from-black from-50% via-indigo-900 via-90% to-purple-800'
       />
       <section
-        className="galaxy bg-galaxy1 absolute h-full w-full bg-auto"
+        className="gal-scroll absolute flex h-full w-full"
         data-value="-5"
-      />
+      >
+        <div className="galaxy w-full bg-galaxy1 bg-auto" data-value="-5" />
+      </section>
       <section
-        className="galaxy bg-galaxy2 absolute h-full w-full bg-auto"
+        className="gal-scroll absolute flex h-full w-full"
         data-value="-1"
-      />
+      >
+        <div className="galaxy w-full bg-galaxy2 bg-auto" data-value="-1" />
+      </section>
       <section
-        className="galaxy bg-galaxy3 absolute h-full w-full bg-auto"
+        className="gal-scroll absolute flex h-full w-full"
         data-value="-3"
-      />
+      >
+        <div className="galaxy w-full bg-galaxy3 bg-auto" data-value="-3" />
+      </section>
       <section
-        className="galaxy bg-galaxy4 absolute h-full w-full bg-auto"
+        className="gal-scroll absolute flex h-full w-full"
         data-value="-9"
-      />
-      <main className="absolute flex h-full w-full items-center justify-center overflow-y-auto">
+      >
+        <div className="galaxy w-full bg-galaxy4 bg-auto" data-value="-9" />
+      </section>
+      <main className="main absolute flex h-full w-full items-center justify-center overflow-y-auto">
         <div className="flex h-[4100px] items-center font-serif text-lg font-semibold text-white">
           Galaxy
         </div>
