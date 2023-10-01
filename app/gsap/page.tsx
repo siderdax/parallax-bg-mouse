3799109740099c8d8dfaba04102ad04584f34daf
KYI <headwaving@gmail.com>
2023-10-01 18:07:31 +0900

    update gsap page

--- app/gsap/page.tsx
+++ app/gsap/page.tsx
@@ -2,25 +2,42 @@
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { useEffect, useLayoutEffect, useRef } from 'react'
-import './style.css'
 
 export default function Page() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
     let ctx = gsap.context(() => {
-      gsap
-        .timeline({
-          scrollTrigger: {
-            trigger: '.shoe',
-            start: 'center center',
-            end: 'bottom top',
-            scrub: true,
-            pin: true,
-            markers: true,
-          },
-        })
-        .from('.midsole', { y: innerHeight * 1.5 })
-        .from('.outsole', { y: innerHeight * 1.5 })
+      const scrollTrigger = {
+        scroller: '.main',
+        trigger: '.word',
+        start: 'center center',
+        end: 'bottom top',
+        scrub: true,
+        markers: true,
+      }
+      const scrollSmoother = {
+        smooth: 2,
+      }
+      gsap.to('.gal-scroll-0', {
+        scrollTrigger,
+        scrollSmoother,
+        y: -50,
+      })
+      gsap.to('.gal-scroll-1', {
+        scrollTrigger,
+        scrollSmoother,
+        y: -70,
+      })
+      gsap.to('.gal-scroll-2', {
+        scrollTrigger,
+        scrollSmoother,
+        y: -90,
+      })
+      gsap.to('.gal-scroll-3', {
+        scrollTrigger,
+        scrollSmoother,
+        y: -110,
+      })
     })
 
     return () => ctx.revert() // cleanup
@@ -28,27 +45,39 @@ export default function Page() {
 
   return (
     <>
-      <h1 className="header-section">Scroll to construct a shoe</h1>
-
-      <section className="shoe">
-        <img
-          src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84da29b46cb1403688b139_outsole.png"
-          className="outsole piece"
-          alt="outsole"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84d9ecb32e46bc5f1f8a8a_component.png?"
-          className="heel piece"
-          alt="heel"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84da293b7f75e5a42bcacd_midsole.png?"
-          className="midsole piece"
-          alt="midsole"
-        />
+      <section
+        className='to-100%" absolute h-full w-full
+        bg-gradient-to-b from-black from-50% via-indigo-900 via-90% to-purple-800'
+      />
+      <section
+        className="gal-scroll-0 absolute flex h-full w-full"
+        data-value="-5"
+      >
+        <div className="galaxy w-full bg-galaxy1 bg-auto" data-value="-5" />
       </section>
-
-      <h1 className="header-section">Cool, right?</h1>
+      <section
+        className="gal-scroll-1 absolute flex h-full w-full"
+        data-value="-1"
+      >
+        <div className="galaxy w-full bg-galaxy2 bg-auto" data-value="-1" />
+      </section>
+      <section
+        className="gal-scroll-2 absolute flex h-full w-full"
+        data-value="-3"
+      >
+        <div className="galaxy w-full bg-galaxy3 bg-auto" data-value="-3" />
+      </section>
+      <section
+        className="gal-scroll-3 absolute flex h-full w-full"
+        data-value="-9"
+      >
+        <div className="galaxy w-full bg-galaxy4 bg-auto" data-value="-9" />
+      </section>
+      <main className="main absolute flex h-full w-full items-center justify-center overflow-y-auto">
+        <div className="word flex h-[4100px] items-center font-serif text-lg font-semibold text-white">
+          Galaxy
+        </div>
+      </main>
     </>
   )
 }
